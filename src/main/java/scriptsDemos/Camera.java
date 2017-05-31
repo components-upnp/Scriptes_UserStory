03ecaf6f43101972f7de6337009d2251f906504d
MKostiuk <Mat01061995>
2017-05-31 16:28:28 +0200

    Ajout script composition démo caméra

---
+++ src/main/java/scriptsDemos/Camera.java
@@ -0,0 +1,73 @@
+package scriptsDemos;
+
+import smac.upnp.wcomp.*;
+
+/**
+ * Created by mkostiuk on 31/05/2017.
+ */
+public class Camera {
+
+    public static void main(String[] args) {
+        ContainerWComp c = new ContainerWComp("container_Structural_0");
+        String nameAdapter = "";
+
+        try {
+            String[] beans = c.getBeans();
+
+            for (String bean : beans) {
+
+                String beanProperties[] = bean.split(":");
+                if (bean.contains("Adapter")) {
+                    nameAdapter = beanProperties[0];
+
+                    String linkFloatX = c.createLink(beanProperties[0], "X_Event", "stringToFloat1", "convert", "");
+                    String linkFloatY = c.createLink(beanProperties[0], "Y_Event", "stringToFloat2", "convert", "");
+
+                    String linkMoveGenVector2D = c.createLink(beanProperties[0], "Move_Event", "oNVIF_ptz_Vector2DFactoryProxy2", "Create_Vector2D_0", "");
+                    String linkMoveGenPtzVEctor = c.createLink(beanProperties[0], "Move_Event", "oNVIF_ptz_PTZVectorFactoryProxy1", "Create_PTZVector_0", "");
+                    String linkMoveCallMove = c.createLink(beanProperties[0], "Move_Event", "pTZ_RelativeMove_CallProxy1", "PTZ_RelativeMove_Trigger", "");
+                }
+            }
+
+            for (String bean : beans) {
+
+
+                String beanProperties[] = bean.split(":");
+                if (bean.contains("android_Remote_Controller")) {
+                    //Ampoule
+                    String ampoule = c.createBeanAtPos(beanProperties[0], beanProperties[1], 600, 300);
+                    pause(1000);
+
+                    //Creation du lien
+                    String linkPoto = c.createLink(beanProperties[0], "Status_Event", nameAdapter, "SetCommande", "");
+                    pause(1000);
+                }
+
+
+            }
+
+
+            pause(3000);
+            c.stopSpy();
+        } catch (NoService noService) {
+            noService.printStackTrace();
+        } catch (NotLaunched notLaunched) {
+            notLaunched.printStackTrace();
+        } catch (SpyNotRunning spyNotRunning) {
+            spyNotRunning.printStackTrace();
+        } catch (NoDevice noDevice) {
+            noDevice.printStackTrace();
+        } catch (ErrorContainer errorContainer) {
+            errorContainer.printStackTrace();
+        }
+
+    }
+
+    public static void pause(long ms){
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e1) {
+            e1.printStackTrace();
+        }
+    }
+}
